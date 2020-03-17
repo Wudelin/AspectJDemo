788eb581ba89954cf1fcb3367832bf9f056b4df2
wdl <wdl@qq.com>
2020-03-17 17:02:22 +0800

    custom plugin

---
+++ aspect/src/main/java/groovy/com/wdl/WdlPlugin.groovy
@@ -0,0 +1,13 @@
+package groovy.com.wdl
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class WdlPlugin implements Plugin<Project> {
+
+    void apply(Project project) {
+        System.out.println("========================");
+        System.out.println("hello gradle plugin!");
+        System.out.println("========================");
+    }
+}
